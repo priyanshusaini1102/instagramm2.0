5f5a6c6d8b3056893cd3cced6d47c7950070d127
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2022-06-30 00:48:53 +0530

    add user detail through index

--- pages/direct.jsx
+++ pages/direct.jsx
@@ -10,21 +10,21 @@ const Direct = () => {
   const { data:session } = useSession();
   
 
-  useEffect(() => {
-    if(session){
+  // useEffect(() => {
+  //   if(session){
 
-      (async () => {
-        await setDoc(doc(db, "users", session?.user?.uid), {
-          email: session?.user?.email,
-          lastSeen: serverTimestamp(),
-          photoURL: session?.user?.image,
-          name:session?.user?.name,
-          username:session?.user?.username
-        },{merge:true});
-      })();
-    }
+  //     (async () => {
+  //       await setDoc(doc(db, "users", session?.user?.uid), {
+  //         email: session?.user?.email,
+  //         lastSeen: serverTimestamp(),
+  //         photoURL: session?.user?.image,
+  //         name:session?.user?.name,
+  //         username:session?.user?.username
+  //       },{merge:true});
+  //     })();
+  //   }
     
-  },[session]);
+  // },[session]);
 
   return (
     <div className="bg-gray-50 h-screen" >
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,29 @@ import Head from 'next/head';
 import BottomBar from '../components/BottomBar';
 import Feed from  '../components/Feed';
 import { useSession } from 'next-auth/react';
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { db } from '../firebase';
+import React,{ useState,useEffect } from 'react';
 
 const Home: NextPage = () => {
 
   const { data: session } = useSession();
+
+  useEffect(() => {
+    if(session){
+
+      (async () => {
+        await setDoc(doc(db, "users", session?.user?.uid), {
+          email: session?.user?.email,
+          lastSeen: serverTimestamp(),
+          photoURL: session?.user?.image,
+          name:session?.user?.name,
+          username:session?.user?.username
+        },{merge:true});
+      })();
+    }
+    
+  },[session]);
 
   return (
     <div className=" bg-gray-50 min-h-screen pb-10 ">
